99b5b678cb047667a80c7f27d196a243a627e301
James Qiu <qiujames@uw.edu>
2018-10-21 00:54:16 -0700

    JS for Dropdown and Search bar

--- search.js
+++ search.js
@@ -1,1 +1,11 @@
-var world = document.getElements
+$(function(){
+
+  $(".dropdown-menu a").click(function(e){
+    var currentId = e.target.id;
+    console.log(currentId);
+    $(".btn:first-child").text($(this).text());
+    $(".btn:first-child").val($(this).text());
+
+  });
+
+});
