b1f3395fb03ee1c42f92bb98911b521209e16b10
ericdyoon <yoone2@cs.washington.edu>
2018-10-20 20:30:22 -0700

    Some more development to API calls
    - add keyword search sources search

--- getjsondata.js
+++ getjsondata.js
@@ -1,17 +1,18 @@
 /*
+    pre: takes in strings keyword and source.
+
+        source is which news outlet to search from.
     post: print out some form of data response from the News API
 */
-function queryNewsAPI(source) {
-    var url = 'https://newsapi.org/v2/everything?' +
-              'q=Apple&' +
-              'sources=' + source +
-              'from=2018-10-21&' +
-              'sortBy=popularity&' +
-              'apiKey=661e892338f046869d509792438cb3c0';
-
+function queryNewsAPI(keyword, source) {
+    // Make a url with query strings and apiKey
+    var url = "https://newsapi.org/v2/top-headlines?" +
+    "q=" + keyword + "&" +
+    "sources=" + source + "&" +
+    "apiKey=661e892338f046869d509792438cb3c0"
 
     console.log(url);
-    
+
     var req = new Request(url);
 
     fetch(req)  // using the MDN Fetch API
@@ -24,6 +25,6 @@ function queryNewsAPI(source) {
         });
 }
 
-function specifySource(source) {
-    queryNewsAPI("bbc-news");
+function specifySource() {
+    queryNewsAPI("trump", "abc-news");
 }
