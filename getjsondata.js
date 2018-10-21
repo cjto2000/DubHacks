a892a582a785313d3184eb95d805e2efaa5afa74
Annalice Ni <annaliceni@gmail.com>
2018-10-20 22:50:41 -0700

    put in maps of bias values and news sources

--- getjsondata.js
+++ getjsondata.js
@@ -1,15 +1,46 @@
+//maps the bias value to source with that value
+var valueToSource = new Map([
+    [1, ["buzzfeed", "the-huffington-post"]],
+    [2, ["new-york-magazine"]],
+    [3, ["the-new-york-times", "the-washington-post", "the-guardian-uk", "politico"]],
+    [4, ["nbc-news", "abc-news", "bbc-news", "usa-today"]],
+    [5, ["reuters", "bloomberg", "cbs-news", "the-economist", "time"]],
+    [6, ["the-wall-street-journal", "the-hill"]],
+    [7, ["the-washington-times"]],
+    [8, ["fox-news", "the-american-conservative"]],
+]);
+
+// maps the news source to its bias value
+var sourceToValue = new Map([
+    ["abc-news", 4],
+    ["bbc-news", 4],
+    ["bloomberg", 5],
+    ["buzzfeed", 1],
+    ["cbs-news", 5],
+    ["fox-news", 8],
+    ["nbc-news", 4],
+    ["new-york-magazine", 2],
+    ["politico", 3],
+    ["reuters", 5],
+    ["the-american-conservative", 8],
+    ["the-economist", 5],
+    ["the-guardian-uk", 3],
+    ["the-hill", 1],
+    ["the-huffington-post", 1],
+    ["the-new-york-times", 3],
+    ["the-wall-street-journal", 6],
+    ["the-washington-post", 3],
+    ["the-washington-times", 7],
+    ["time", 5],
+    ["usa-today", 4],
+]);
+
 /*
     pre: takes in strings keyword and source.
 
         source is which news outlet to search from.
     post: print out some form of data response from the News API
 */
-var sourceMap = new Map([
-    [1, ['buzzfeed', 'the-huffington-post']],
-    [2, ['new-york-magazine']],
-    [3, ['the-new-york-times', 'the-washington-post']],
-]);
-
 function queryNewsAPI(keyword, source) {
     // Make a url with query strings and apiKey
     var url = "https://newsapi.org/v2/top-headlines?" +
@@ -30,7 +61,8 @@ function queryNewsAPI(keyword, source) {
             document.getElementById("article1").innerHTML = (response.articles[0].content);
         });
 
-    console.log(sourceMap);
+    console.log(valueToSource);
+    console.log(sourceToValue);
 }
 
 function specifySource() {
