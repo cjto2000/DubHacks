8e6a75aa10ac71b7c8691997bc045c5d370dc5b6
Eric Donghoon Yoon <43554323+ericdyoon@users.noreply.github.com>
2018-10-20 23:19:17 -0700

    Make indents prettier (#7)
    * Pretty indents
    
    * A small demo of the API call
    
    * Robust api call and Iframes

--- getjsondata.js
+++ getjsondata.js
@@ -43,7 +43,7 @@ var sourceToValue = new Map([
 */
 function queryNewsAPI(keyword, source) {
     // Make a url with query strings and apiKey
-    var url = "https://newsapi.org/v2/top-headlines?" +
+    var url = "https://newsapi.org/v2/everything?" +
     "q=" + keyword + "&" +
     "sources=" + source + "&" +
     "apiKey=661e892338f046869d509792438cb3c0";
@@ -58,7 +58,11 @@ function queryNewsAPI(keyword, source) {
         })
         .then(function (response) {  // Processing to get the json from the response
             console.log(response);
-            document.getElementById("article1").innerHTML = (response.articles[0].content);
+            if (response.articles.length > 0) {
+                document.getElementById("article1").setAttribute("src", response.articles[0].url);
+            } else {
+                document.getElementById("article1").innerHTML = "No articles found.";
+            }
         });
 
     console.log(valueToSource);
