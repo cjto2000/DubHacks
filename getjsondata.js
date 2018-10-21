135fa8ac456b3875b1ca4cddb7c5614ee4c9ee8e
ericdyoon <yoone2@cs.washington.edu>
2018-10-20 19:56:56 -0700

    API calls going

--- getjsondata.js
+++ getjsondata.js
@@ -1,14 +1,29 @@
-function queryNewsAPI() {
+/*
+    post: print out some form of data response from the News API
+*/
+function queryNewsAPI(source) {
     var url = 'https://newsapi.org/v2/everything?' +
               'q=Apple&' +
+              'sources=' + source +
               'from=2018-10-21&' +
               'sortBy=popularity&' +
               'apiKey=661e892338f046869d509792438cb3c0';
 
+
+    console.log(url);
+    
     var req = new Request(url);
 
-    fetch(req)
-        .then(function(response) {
-            console.log(response.json());
+    fetch(req)  // using the MDN Fetch API
+        .then(function(response) {  // Processing the response object from request
+            return response.json();
+        })
+        .then(function (response) {  // Processing to get the json from the response
+            console.log(response);
+            document.getElementById("article1").innerHTML = (response.articles[0].content);
         });
+}
+
+function specifySource(source) {
+    queryNewsAPI("bbc-news");
 }
